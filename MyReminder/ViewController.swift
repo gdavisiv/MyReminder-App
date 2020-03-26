e0a863a5a88fa9829d92460e408ee9b736481052
gdavisiv <gdavisiv@gmail.com>
2020-03-26 13:00:31 -0400

    Creating the TableView/Array/Variables needed in ViewController

--- MyReminder/ViewController.swift
+++ MyReminder/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Setting up the table View
+    @IBOutlet var table: UITableView!
+    
+    // Variable models will hold an array myReminders and start off Empty
+    var models = [MyReminder]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    //Implement our basic tableview functions
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+}
+
+//
+struct MyReminder {
+    let title: String
+    let date: Date
+    let indentifier: String
+}
