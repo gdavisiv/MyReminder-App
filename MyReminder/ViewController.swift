c9f5090b2f58af257653d741248fd596a8aafb37
gdavisiv <gdavisiv@gmail.com>
2020-03-26 12:27:21 -0400

    Initial Commit

---
+++ MyReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyReminder
+//
+//  Created by user163072 on 3/26/20.
+//  Copyright © 2020 George Davis IV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
